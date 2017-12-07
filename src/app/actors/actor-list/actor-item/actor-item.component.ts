2727c0c42141a5beca6d42a2b96982e99db5b78a
Tomvh123 <thaaster@avans.nl>
2017-12-07 13:57:59 +0100

    Added actor List pagina

---
+++ src/app/actors/actor-list/actor-item/actor-item.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Actor} from '../../../actor.model';
+
+
+@Component({
+  selector: 'app-actor-item',
+  templateUrl: './actor-item.component.html',
+  styleUrls: ['./actor-item.component.css']
+})
+export class ActorItemComponent implements OnInit {
+  @Input() actor: Actor;
+  @Input() index: string
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.index = this.actor._id;
+  }
+
+}
---
+++ src/app/actors/actor-list/actor-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Actor} from '../../actor.model';
+import {ActorService} from '../actor-service.service';
+
+@Component({
+  selector: 'app-actor-list',
+  templateUrl: './actor-list.component.html',
+  styleUrls: ['./actor-list.component.css']
+})
+export class ActorListComponent implements OnInit, OnDestroy {
+  actors: Actor[];
+  subscription: Subscription;
+
+  constructor(private actorService: ActorService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.subscription = this.actorService.actorChanged
+      .subscribe(
+        (actors: Actor[]) => {
+          this.actorService.getActors()
+            .then(res => {
+              this.actors = res;
+            });
+        }
+      );
+    this.actorService.getActors().then(res => {
+      this.actors = res;
+    });
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
