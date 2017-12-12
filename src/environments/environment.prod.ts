3fdb411be400842578fabac0a52c56814df58d08
tomvh123 <thaaster@avans.nl>
2017-12-12 01:31:13 +0100

    package.json changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   serverUrl: 'https://mongo-db-server-tom.herokuapp.com/api/v1',
   serverUrlChar: 'https://mongo-db-server-tom.herokuapp.com/api/v1/characters/',
   serverUrlActor: 'https://mongo-db-server-tom.herokuapp.com/api/v1/actors/',
-  serverUrlRel: 'https://mongo-db-server-tom.herokuapp.com/api/v1seriesrel/'
+  serverUrlRel: 'https://mongo-db-server-tom.herokuapp.com/api/v1/seriesrel/'
 };
