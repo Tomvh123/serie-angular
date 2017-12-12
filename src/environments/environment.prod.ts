0995582258c7df0a335cff56bd12580eb8334fd8
tomvh123 <thaaster@avans.nl>
2017-12-12 01:21:28 +0100

    package.json changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   serverUrl: 'https://mongo-db-server-tom.herokuapp.com/api/v1',
   serverUrlChar: 'https://mongo-db-server-tom.herokuapp.com/api/v1/characters/',
-  serverUrlActor: 'https://mongo-db-server-tom.herokuapp.com/api/v1/actors/'
+  serverUrlActor: 'https://mongo-db-server-tom.herokuapp.com/api/v1/actors/',
+  serverUrlRel: 'https://mongo-db-server-tom.herokuapp.com/api/v1seriesrel/'
 };
